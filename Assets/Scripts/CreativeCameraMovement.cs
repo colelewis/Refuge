8985db21da97f781ef53c9b072bd14a2e288bfb2
Cole Lewis <cole.g.lewis@gmail.com>
2022-10-27 17:00:18 -0400

    debug player

---
+++ Assets/Scripts/CreativeCameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CreativeCameraMovement : MonoBehaviour
+{
+    private const float YMin = -50.0f;
+    private const float YMax = 50.0f;
+ 
+    public Transform lookAt;
+    public Transform player;
+ 
+    public float distance = 10.0f; // this is distance from the player; 1st person POV -> distance very low, 3rd person POV -> distance higher, around 200
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity = 4.0f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+ 
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        currentY += Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+ 
+        Vector3 Direction = new Vector3(0, 0, distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * Direction;
+ 
+        transform.LookAt(lookAt.position);
+    }
+}
