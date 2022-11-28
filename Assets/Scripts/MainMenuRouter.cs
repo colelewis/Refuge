8fb5154cacee171dea47aa31bcb34d04d82bd27a
Cole Lewis <cole.g.lewis@gmail.com>
2022-11-28 10:22:41 -0500

    Main menu work

---
+++ Assets/Scripts/MainMenuRouter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRouter : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject HowToPlayPanel;
+    public GameObject AboutPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu();
+    }
+
+    public void mainMenu() {
+        MainMenu.SetActive(true);
+    }
+
+    public void Load() {
+        // load save state
+    }
+
+    public void HowToPlay() {
+        HowToPlayPanel.SetActive(true);
+    }
+
+    public void About() {
+        AboutPanel.SetActive(true);
+    }
+
+    public void NewGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("IslandScene");
+    }
+
+}
